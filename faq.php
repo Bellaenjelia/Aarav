e11142d24bbe4dc8431496c205f22c5ee67a6395
amirashafwa <nadauser3@gmail.com>
2024-02-21 14:52:58 +0700

    mengubah faq

--- faq.php
+++ faq.php
@@ -46,13 +46,13 @@
                     <div class="accordion-item" data-aos="fade-left">
                         <h2 class="accordion-header" id="headingThree">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Does it need to paid?
+                            Apakah saya perlu membayar?
                         </button>
                         </h2>
 
                         <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                Tidak. Semua layanan dalam website kami dapat diakses secara gratis sehingga anda tidak perlu mengeluarkan biaya sedikitpun.
                             </div>
                         </div>
                     </div>
