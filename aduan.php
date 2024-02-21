1a2969641c95dadb712470cb8c2ff629e6200839
syafnaa <syafnamarwan@gmail.com>
2024-02-21 14:39:18 +0700

    CREATE

--- aduan.php
+++ aduan.php
@@ -1,5 +1,27 @@
 <!-- ======= Aduan Section ======= -->
-<section id="aduan" class="contact page">
+<?php
+if (isset($_POST['bsimpan'])) {
+    $tgl_aduan = date('Y-m-d h:i:s');
+    $tgl_kejadian = $_POST['tgl_kejadian'];
+    $lokasi = $_POST['lokasi'];
+    $isi = $_POST['isi'];
+    
+    $foto = $_FILES['foto'];
+    $nama_foto = $foto['name'];
+    move_uploaded_file($foto['tmp_name'], "bukti/".$foto['name']);
+
+    $query = mysqli_query($koneksi, "INSERT INTO aduan (isi, tgl_aduan, tgl_kejadian, lokasi, foto) VALUES ('$isi', '$tgl_aduan', '$tgl_kejadian', '$lokasi', '$nama_foto')");
+
+    if ($query) {
+        echo "<script>alert('Tambah data berhasil');document.location='index.php?page=aduan';</script>";
+    } else {
+        echo "<script>alert('Tambah data gagal');document.location='index.php?page=aduan';</script>";
+    }
+}
+?>
+
+
+<section id="aduan" class="aduan page">
     <div class="container" data-aos="fade-up">
 
     <div class="section-title m-3">
@@ -10,31 +32,22 @@
     <div class="row">
 
         <div class="col-lg-6" style="margin-left: 3rem;">
-        <form action="forms/aduan.php" method="post" role="form" class="php-email-form" data-aos="fade-up">
+        <form method="post" role="form" data-aos="fade-up" enctype="multipart/form-data">
             <div class="form-group">
-            <label class="col-sm-7 col-form-label" style="color: black;">Tanggal Aduan :</label>
-            <input placeholder="Tanggal Aduan" type="date" name="tgl_aduan" class="form-control" id="tgl_aduan" required>
+                <label class="col-sm-7 col-form-label" style="color: black;">Tanggal Kejadian :</label>
+                <input placeholder="tgl_kejadian" type="date" class="form-control" name="tgl_kejadian" id="email" required>
             </div>
             <div class="form-group mt-3">
-            <label class="col-sm-7 col-form-label" style="color: black;">Tanggal Kejadian :</label>
-            <input placeholder="tgl_kejadian" type="date" class="form-control" name="tgl_kejadian" id="email" required>
+                <label class="col-sm-7 col-form-label" style="color: black;">Lokasi :</label>
+                <input placeholder="Lokasi" type="text" class="form-control" name="lokasi" id="lokasi" required>
             </div>
             <div class="form-group mt-3">
-            <label class="col-sm-7 col-form-label" style="color: black;">Lokasi :</label>
-            <input placeholder="Lokasi" type="text" class="form-control" name="lokasi" id="lokasi" required>
+                <label class="col-sm-7 col-form-label" style="color: black;">Aduan :</label>
+                <textarea placeholder="Isi Aduan" class="form-control" name="isi" rows="5" required></textarea>
             </div>
             <div class="form-group mt-3">
-            <label class="col-sm-7 col-form-label" style="color: black;">Aduan :</label>
-            <textarea placeholder="Isi Aduan" class="form-control" name="isi" rows="5" required></textarea>
-            </div>
-            <div class="form-group mt-3">
-            <label class="col-sm-7 col-form-label" style="color: black;">Bukti :</label>
-            <input placeholder="Kirim Bukti" type="file" class="form-control" name="foto"id="foto" required>
-            </div>
-            <div class="my-3">
-            <div class="loading">Loading</div>
-            <div class="error-message"></div>
-            <div class="sent-message">Your message has been sent. Thank you!</div>
+                <label class="col-sm-7 col-form-label" style="color: black;">Bukti :</label>
+                <input placeholder="Kirim Bukti" type="file" class="form-control" name="foto"id="foto" required>
             </div>
             <div class="text-center"><button type="submit" name="bsimpan">Kirim Aduan</button></div>
         </form>
