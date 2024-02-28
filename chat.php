440f376acba8fc67ed3b1bbe3f8448c4c78a839c
amirashafwa <nadauser3@gmail.com>
2024-02-28 13:08:12 +0700

    update konseling page

---
+++ chat.php
@@ -0,0 +1,61 @@
+<section class="chat page">
+    <div class="container">
+        <div class="card chat-card">
+            <div class="card-body">
+                <div class="d-flex align-items-center mb-3">
+                    <a href="index.php" class="back-icon"><i class="bi bi-arrow-left"></i></a>
+                    <img src="assets/img/apple-touch-icon.png" alt="" width="40" height="40" class="rounded-circle ms-3 me-2">
+                    <div class="nama-guru">
+                        <span>Nama Guru</span>
+                    </div>
+                </div>
+                <div class="messenger-box chat-box py-5">
+                    <ul>
+                        <li>
+                            <div class="msg-received msg-container">
+                                <div class="avatar">
+                                    <img src="admin-web/images/avatar/64-1.jpg" alt="">
+                                    <div class="send-time">11.11 am</div>
+                                </div>
+                                <div class="msg-box">
+                                    <div class="inner-box">
+                                        <div class="name">
+                                            John Doe
+                                        </div>
+                                        <div class="meg">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis sunt placeat velit ad reiciendis ipsam
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- /.msg-received -->
+                        </li>
+                        <li>
+                            <div class="msg-sent msg-container">
+                                <div class="avatar">
+                                    <img src="admin-web/images/avatar/64-2.jpg" alt="">
+                                    <div class="send-time">11.11 am</div>
+                                </div>
+                                <div class="msg-box">
+                                    <div class="inner-box">
+                                        <div class="name">
+                                            John Doe
+                                        </div>
+                                        <div class="meg">
+                                            Hay how are you doing?
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- /.msg-sent -->
+                        </li>
+                    </ul>
+                </div>
+                <div class="typing-area">
+                    <textarea id="message" class="form-control"></textarea>
+                    <button id="sendBtn">
+                    <i class="bi bi-send"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- konseling.php
+++ konseling.php
@@ -1,60 +1,42 @@
-<section class="chat page">
-    <div class="container">
-        <div class="card chat-card">
+<section class="chat page d-flex justify-content-center align-items-center">
+    <div class="container w-400">
+        <div class="card konseling-card">
             <div class="card-body">
-                <div class="d-flex align-items-center mb-3">
-                    <a href="index.php" class="back-icon"><i class="bi bi-arrow-left"></i></a>
-                    <img src="assets/img/apple-touch-icon.png" alt="" width="40" height="40" class="rounded-circle ms-3 me-2">
-                    <div class="nama-guru">
-                        <span>Nama Guru</span>
-                    </div>
-                </div>
-                <div class="messenger-box chat-box py-5">
-                    <ul>
-                        <li>
-                            <div class="msg-received msg-container">
-                                <div class="avatar">
-                                    <img src="admin-web/images/avatar/64-1.jpg" alt="">
-                                    <div class="send-time">11.11 am</div>
-                                </div>
-                                <div class="msg-box">
-                                    <div class="inner-box">
-                                        <div class="name">
-                                            John Doe
-                                        </div>
-                                        <div class="meg">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis sunt placeat velit ad reiciendis ipsam
-                                        </div>
-                                    </div>
-                                </div>
-                            </div><!-- /.msg-received -->
-                        </li>
-                        <li>
-                            <div class="msg-sent msg-container">
-                                <div class="avatar">
-                                    <img src="admin-web/images/avatar/64-2.jpg" alt="">
-                                    <div class="send-time">11.11 am</div>
-                                </div>
-                                <div class="msg-box">
-                                    <div class="inner-box">
-                                        <div class="name">
-                                            John Doe
-                                        </div>
-                                        <div class="meg">
-                                            Hay how are you doing?
-                                        </div>
-                                    </div>
-                                </div>
-                            </div><!-- /.msg-sent -->
-                        </li>
-                    </ul>
-                </div>
-                <div class="typing-area">
-                    <textarea id="message" class="form-control"></textarea>
-                    <button id="sendBtn">
-                    <i class="bi bi-send"></i>
-                    </button>
-                </div>
+                <ul id="chatList" class="overflow-auto">
+                    <li class="list-group-item">
+                        <a href="?page=chat" class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex align-items-center nama-guru">
+                                <img src="assets/img/apple-touch-icon.png" class="w-10 rounded-circle">
+                                <span class="fs-xs m-2"> Nama Guru<br>
+                                <small>
+                                    chat
+                                </small>            	
+                            </div>
+                        </a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="?page=chat" class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex align-items-center nama-guru">
+                                <img src="assets/img/apple-touch-icon.png" class="w-10 rounded-circle">
+                                <span class="fs-xs m-2"> Nama Guru<br>
+                                <small>
+                                    chat
+                                </small>            	
+                            </div>
+                        </a>
+                    </li>
+                    <li class="list-group-item">
+                        <a href="?page=chat" class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex align-items-center nama-guru">
+                                <img src="assets/img/apple-touch-icon.png" class="w-10 rounded-circle">
+                                <span class="fs-xs m-2"> Nama Guru<br>
+                                <small>
+                                    chat
+                                </small>            	
+                            </div>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
