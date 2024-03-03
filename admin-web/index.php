2a2de58a22af755fc905d6afaf38e69ff744a06e
amirashafwa <nadauser3@gmail.com>
2024-03-03 17:28:03 +0700

    update js

--- admin-web/index.php
+++ admin-web/index.php
@@ -121,7 +121,9 @@ include '../chat/helpers/last_chat.php';
                     <li class="menu-title">LAYANAN</li><!-- /.menu-title -->
                     <li class="menu-item">
                         <a href="index.php?page=konseling"><i class="menu-icon fa fa-comment-o"></i>Konseling</a>
-                        <a href="index.php?page=aduan"><i class="menu-icon fa fa-volume-up"></i>Aduan</a>
+                    </li>
+                    <li class="menu-item">
+                    <a href="index.php?page=aduan"><i class="menu-icon fa fa-volume-up"></i>Aduan</a>
                     </li>
                 </ul>
             </div><!-- /.navbar-collapse -->
@@ -264,7 +266,6 @@ include '../chat/helpers/last_chat.php';
     <!-- /#right-panel -->
 
     <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
@@ -288,6 +289,9 @@ include '../chat/helpers/last_chat.php';
     <script src="https://cdn.jsdelivr.net/npm/moment@2.22.2/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.js"></script>
     <script src="assets/js/init/fullcalendar-init.js"></script>
+
+    <!--Script AJAX-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <!--Local Stuff-->
     <script>
